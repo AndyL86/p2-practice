613dc31ff327907b78e869b28a9b266999cf91d9
AndyL86 <alinehan89@outlook.com>
2022-08-01 15:06:14 +0000

    style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,19 @@
 // Setting the scores and selecting our HTML elements.
 let compWins = 1;
-let userWIns = 1;
+let userWins = 1;
+
 const userScore = document.getElementById('userCount');
 const compScore = document.getElementById('compCount');
 const buttons = document.querySelectorAll('.selection button');
 const userImage = document.querySelector('.image i');
 const compImage = document.querySelector('.computer i');
-// The gameImages array below this contains the rock,paper, scissors, lizard and spock images.
+// The gameImages array below contains the rock,paper, scissors, lizard and spock images.
 
 const gameImages = ["fas fa-hand-rock", "fas fa-hand-paper","fas fa-hand-scissors", "fas fa-hand-lizard", "fas fa-hand-spock"];
 const result = document.getElementById('results');
 
 // Game Functionality: Setting forEach function for the buttons.
-const game = () => {
+function game() {
     buttons.forEach(btn => {
         btn.addEventListener('click', (e) => {
             // Random rock paper scissor lizard spock for the computer and the user
@@ -30,8 +31,8 @@ const game = () => {
             }
             // if it's win or lose.
             else if (userImage.className === gameImages[0] && compImage.className === gameImages[2]) {
-                userScore.innerHTML = userWIns;
-                userWIns++;
+                userScore.innerHTML = userWins;
+                userWins++;
                 result.innerHTML = "Congratulations! You Won! ";
                 result.style.color = 'green';
             } else if (userImage.className === gameImages[0] && compImage.className === gameImages[1]) {
@@ -45,8 +46,8 @@ const game = () => {
                 result.innerHTML = "Sorry, you lose! ";
                 result.style.color = 'red';
             } else if (userImage.className === gameImages[1] && compImage.className === gameImages[0]) {
-                userScore.innerHTML = userWIns;
-                userWIns++;
+                userScore.innerHTML = userWins;
+                userWins++;
                 result.innerHTML = "Congratulations! You Won! ";
                 result.style.color = 'green';
             } else if (userImage.className === gameImages[2] && compImage.className === gameImages[0]) {
@@ -54,44 +55,44 @@ const game = () => {
                 compWins++;
                 result.innerHTML = "Sorry, you lose! ";
                 result.style.color = 'red';
-            } else if (userImage.className === gameImages[2] && compimage.className === gameImages[1]) {
-                userScore.innerHTML = userWIns;
-                userWIns++;
+            } else if (userImage.className === gameImages[2] && compImage.className === gameImages[1]) {
+                userScore.innerHTML = userWins;
+                userWins++;
                 result.innerHTML = "Congratulations! You Won! ";
                 result.style.color = 'green';
-            } else if (userImage.className === gameImages[0] && compimage.className === gameImages[3]) {
-                userScore.innerHTML = userWIns;
-                userWIns++;
+            } else if (userImage.className === gameImages[0] && compImage.className === gameImages[3]) {
+                userScore.innerHTML = userWins;
+                userWins++;
                 result.innerHTML = "Congratulations! You Won! ";
                 result.style.color = 'green';
-            } else if (userImage.className === gameImages[3] && compimage.className === gameImages[4]) {
-                userScore.innerHTML = userWIns;
-                userWIns++;
+            } else if (userImage.className === gameImages[3] && compImage.className === gameImages[4]) {
+                userScore.innerHTML = userWins;
+                userWins++;
                 result.innerHTML = "Congratulations! You Won! ";
                 result.style.color = 'green';
-            } else if (userImage.className === gameImages[4] && compimage.className === gameImages[2]) {
-                userScore.innerHTML = userWIns;
-                userWIns++;
+            } else if (userImage.className === gameImages[4] && compImage.className === gameImages[2]) {
+                userScore.innerHTML = userWins;
+                userWins++;
                 result.innerHTML = "Congratulations! You Won! ";
                 result.style.color = 'green';
-            } else if (userImage.className === gameImages[2] && compimage.className === gameImages[3]) {
-                userScore.innerHTML = userWIns;
-                userWIns++;
+            } else if (userImage.className === gameImages[2] && compImage.className === gameImages[3]) {
+                userScore.innerHTML = userWins;
+                userWins++;
                 result.innerHTML = "Congratulations! You Won! ";
                 result.style.color = 'green';
-            } else if (userImage.className === gameImages[3] && compimage.className === gameImages[1]) {
-                userScore.innerHTML = userWIns;
-                userWIns++;
+            } else if (userImage.className === gameImages[3] && compImage.className === gameImages[1]) {
+                userScore.innerHTML = userWins;
+                userWins++;
                 result.innerHTML = "Congratulations! You Won! ";
                 result.style.color = 'green';
-            } else if (userImage.className === gameImages[1] && compimage.className === gameImages[4]) {
-                userScore.innerHTML = userWIns;
-                userWIns++;
+            } else if (userImage.className === gameImages[1] && compImage.className === gameImages[4]) {
+                userScore.innerHTML = userWins;
+                userWins++;
                 result.innerHTML = "Congratulations! You Won! ";
                 result.style.color = 'green';
-            } else if (userImage.className === gameImages[4] && compimage.className === gameImages[0]) {
-                userScore.innerHTML = userWIns;
-                userWIns++;
+            } else if (userImage.className === gameImages[4] && compImage.className === gameImages[0]) {
+                userScore.innerHTML = userWins;
+                userWins++;
                 result.innerHTML = "Congratulations! You Won! ";
                 result.style.color = 'green';
             } else if (userImage.className === gameImages[3] && compImage.className === gameImages[0]) {
@@ -132,7 +133,7 @@ const game = () => {
             }
         });
     });
-}
+};
 // Calling the function.
 game();
 // This function contains all the game logic.
