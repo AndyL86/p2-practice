ade69bc6caacfc09e2d7f6db354619e517c584b3
AndyL86 <alinehan89@outlook.com>
2022-08-01 12:28:55 +0000

    style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,15 @@
 // Setting the scores and selecting our HTML elements.
 let compWins = 1;
 let userWIns = 1;
-const userScore = document.getElementById('userScore');
-const compScore = document.getElementById('compScore');
+const userScore = document.getElementById('userCount');
+const compScore = document.getElementById('compCount');
 const buttons = document.querySelectorAll('.selection button');
 const userImage = document.querySelector('.image i');
 const compImage = document.querySelector('.computer i');
 // The gameImages array below this contains the rock,paper, scissors, lizard and spock images.
 
 const gameImages = ["fas fa-hand-rock", "fas fa-hand-paper","fas fa-hand-scissors", "fas fa-hand-lizard", "fas fa-hand-spock"];
-const text = document.getElementById('demo');
-const text2 = document.getElementById('demo2');
+const result = document.getElementById('results');
 
 // Game Functionality: Setting forEach function for the buttons.
 const game = () => {
@@ -22,127 +21,114 @@ const game = () => {
             let randomNum = Math.floor(Math.random() * gameImages.length);
             compImage.className = gameImages[randomNum];
             // Game Score.
-            // If it's a Tie .
+            // If it's a tied game .
             if (userImage.className === compImage.className) {
                 userScore.innerHTML = userScore.innerHTML;
                 compScore.innerHTML = compScore.innerHTML;
-                text.innerHTML = "Tied Game!! ";
-                text2.innerHTML = text.innerHTML;
+                result.innerHTML = "Tied Game!! ";
+                result.style.color = 'blue';
             }
             // if it's win or lose.
             else if (userImage.className === gameImages[0] && compImage.className === gameImages[2]) {
                 userScore.innerHTML = userWIns;
                 userWIns++;
-                text.innerHTML = "Congratulations! You Won! ";
-                text2.innerHTML = text.innerHTML;
+                result.innerHTML = "Congratulations! You Won! ";
+                result.style.color = 'green';
             } else if (userImage.className === gameImages[0] && compImage.className === gameImages[1]) {
                 compScore.innerHTML = compWins;
                 compWins++;
-                text.innerHTML = "Sorry, you lose! ";
-                text2.innerHTML = text.innerHTML;
+                result.innerHTML = "Sorry, you lose! ";
+                result.style.color = 'red';
             } else if (userImage.className === gameImages[1] && compImage.className === gameImages[2]) {
                 compScore.innerHTML = compWins;
                 compWins++;
-                text.innerHTML = "Sorry, you lose! ";
-                text2.innerHTML = text.innerHTML;
+                result.innerHTML = "Sorry, you lose! ";
+                result.style.color = 'red';
             } else if (userImage.className === gameImages[1] && compImage.className === gameImages[0]) {
                 userScore.innerHTML = userWIns;
                 userWIns++;
-                text.innerHTML = "Congratulations! You Won! ";
-                text2.innerHTML = text.innerHTML;
+                result.innerHTML = "Congratulations! You Won! ";
+                result.style.color = 'green';
             } else if (userImage.className === gameImages[2] && compImage.className === gameImages[0]) {
                 compScore.innerHTML = compWins;
                 compWins++;
-                text.innerHTML = "Sorry, you lose! ";
-                text2.innerHTML = text.innerHTML;
+                result.innerHTML = "Sorry, you lose! ";
+                result.style.color = 'red';
             } else if (userImage.className === gameImages[2] && compimage.className === gameImages[1]) {
                 userScore.innerHTML = userWIns;
                 userWIns++;
-                text.innerHTML = "Congratulations! You Won! ";
-                text2.innerHTML = text.innerHTML;
+                result.innerHTML = "Congratulations! You Won! ";
+                result.style.color = 'green';
             } else if (userImage.className === gameImages[0] && compimage.className === gameImages[3]) {
                 userScore.innerHTML = userWIns;
                 userWIns++;
-                text.innerHTML = "Congratulations! You Won! ";
-                text2.innerHTML = text.innerHTML;
-
+                result.innerHTML = "Congratulations! You Won! ";
+                result.style.color = 'green';
             } else if (userImage.className === gameImages[3] && compimage.className === gameImages[4]) {
                 userScore.innerHTML = userWIns;
                 userWIns++;
-                text.innerHTML = "Congratulations! You Won! ";
-                text2.innerHTML = text.innerHTML;
-
+                result.innerHTML = "Congratulations! You Won! ";
+                result.style.color = 'green';
             } else if (userImage.className === gameImages[4] && compimage.className === gameImages[2]) {
                 userScore.innerHTML = userWIns;
                 userWIns++;
-                text.innerHTML = "Congratulations! You Won! ";
-                text2.innerHTML = text.innerHTML;
-
+                result.innerHTML = "Congratulations! You Won! ";
+                result.style.color = 'green';
             } else if (userImage.className === gameImages[2] && compimage.className === gameImages[3]) {
                 userScore.innerHTML = userWIns;
                 userWIns++;
-                text.innerHTML = "Congratulations! You Won! ";
-                text2.innerHTML = text.innerHTML;
-
+                result.innerHTML = "Congratulations! You Won! ";
+                result.style.color = 'green';
             } else if (userImage.className === gameImages[3] && compimage.className === gameImages[1]) {
                 userScore.innerHTML = userWIns;
                 userWIns++;
-                text.innerHTML = "Congratulations! You Won! ";
-                text2.innerHTML = text.innerHTML;
-
+                result.innerHTML = "Congratulations! You Won! ";
+                result.style.color = 'green';
             } else if (userImage.className === gameImages[1] && compimage.className === gameImages[4]) {
                 userScore.innerHTML = userWIns;
                 userWIns++;
-                text.innerHTML = "Congratulations! You Won! ";
-                text2.innerHTML = text.innerHTML;
-
+                result.innerHTML = "Congratulations! You Won! ";
+                result.style.color = 'green';
             } else if (userImage.className === gameImages[4] && compimage.className === gameImages[0]) {
                 userScore.innerHTML = userWIns;
                 userWIns++;
-                text.innerHTML = "Congratulations! You Won! ";
-                text2.innerHTML = text.innerHTML;
-
+                result.innerHTML = "Congratulations! You Won! ";
+                result.style.color = 'green';
             } else if (userImage.className === gameImages[3] && compImage.className === gameImages[0]) {
                 compScore.innerHTML = compWins;
                 compWins++;
-                text.innerHTML = "Sorry, you lose! ";
-                text2.innerHTML = text.innerHTML;
-
+                result.innerHTML = "Sorry, you lose! ";
+                result.style.color = 'red';
             } else if (userImage.className === gameImages[3] && compImage.className === gameImages[2]) {
                 compScore.innerHTML = compWins;
                 compWins++;
-                text.innerHTML = "Sorry, you lose! ";
-                text2.innerHTML = text.innerHTML;
-
+                result.innerHTML = "Sorry, you lose! ";
+                result.style.color = 'red';
             } else if (userImage.className === gameImages[4] && compImage.className === gameImages[1]) {
                 compScore.innerHTML = compWins;
                 compWins++;
-                text.innerHTML = "Sorry, you lose! ";
-                text2.innerHTML = text.innerHTML;
-
+                result.innerHTML = "Sorry, you lose! ";
+                result.style.color = 'red';
             } else if (userImage.className === gameImages[4] && compImage.className === gameImages[3]) {
                 compScore.innerHTML = compWins;
                 compWins++;
-                text.innerHTML = "Sorry, you lose! ";
-                text2.innerHTML = text.innerHTML;
-
+                result.innerHTML = "Sorry, you lose! ";
+                result.style.color = 'red';
             } else if (userImage.className === gameImages[2] && compImage.className === gameImages[4]) {
                 compScore.innerHTML = compWins;
                 compWins++;
-                text.innerHTML = "Sorry, you lose! ";
-                text2.innerHTML = text.innerHTML;
-
+                result.innerHTML = "Sorry, you lose! ";
+                result.style.color = 'red';
             } else if (userImage.className === gameImages[1] && compImage.className === gameImages[3]) {
                 compScore.innerHTML = compWins;
                 compWins++;
-                text.innerHTML = "Sorry, you lose! ";
-                text2.innerHTML = text.innerHTML;
-
+                result.innerHTML = "Sorry, you lose! ";
+                result.style.color = 'red';
             } else if (userImage.className === gameImages[0] && compImage.className === gameImages[4]) {
                 compScore.innerHTML = compWins;
                 compWins++;
-                text.innerHTML = "Sorry, you lose! ";
-                text2.innerHTML = text.innerHTML;
+                result.innerHTML = "Sorry, you lose! ";
+                result.style.color = 'red';
             }
         });
     });
